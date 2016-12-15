f335de21079fc9233c43bc68958d740b9a17b80e
Hilau <hugo.huilier@viacesi.fr>
2016-12-15 14:33:19 +0100

    Ajout du design

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="fr" data-token="{{ csrf_token() }}">
-<<<<<<< Updated upstream
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,6 +11,9 @@
     <!-- Bootstrap -->
     <link href="http://localhost/prometheus/public/css/bootstrap.min.css" rel="stylesheet">
     <link href="http://localhost/prometheus/public/css/style.css" rel="stylesheet">
+    @if(Route::currentRouteName() == 'users.trombinoscope')
+		<link href="http://localhost/prometheus/public/css/trombinoscope.css" rel="stylesheet">
+	@endif
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -21,38 +22,12 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>	
-		<script src="http://localhost/prometheus/public/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="http://localhost/prometheus/public/js/trombinoscope.js"></script>	
-=======
-	<head>		
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		<title>Prometheus</title>
-		
-		<link rel="shortcut icon" href="{{ asset('logo.png') }}">
-		<!-- Bootstrap -->
-		<link href="http://localhost/prometheus/public/css/bootstrap.min.css" rel="stylesheet">
-		<link href="http://localhost/prometheus/public/css/style.css" rel="stylesheet">
-		@if(Route::currentRouteName() == 'users.trombinoscope')
-			<link href="http://localhost/prometheus/public/css/trombinoscope.css" rel="stylesheet">
-		@endif
-		
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-		
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		@if(Route::currentRouteName() == 'users.trombinoscope')			
 			<script type="text/javascript" src="http://localhost/prometheus/public/js/jquery.autocomplete.min.js"></script>
 			<script type="text/javascript" src="http://localhost/prometheus/public/js/trombinoscope.js"></script>	
 		@endif
->>>>>>> Stashed changes
+		<script src="http://localhost/prometheus/public/js/bootstrap.min.js"></script>		
 	</head>
 
 <body>
@@ -95,45 +70,29 @@
     @yield('contenu')
 
     <footer class="footer">
-    	<div class="container">
-    		<div class="row">
-		        <div class="col-md-4">
-		            <!-- <img class="logo" src="http://localhost/prometheus/public/logo.png" alt="logo"> -->
-		        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    
+                </div>
 
-<<<<<<< Updated upstream
-        
-	            <div class="col-md-4">
-=======
+                <div class="col-md-4">
+                    <a href="#">Contact</a><br>
+                    <a href="#">Adresse des centres </a><br>
+                    <a href="#">Nous contacter </a><br>
+                    <a href="#">E-mail </a>
+                </div>
 
-			</div>
-		</header>
-	<body>
-		@yield('contenu')
-
-		<footer>
-	        <div class="col-md-2">
-	            <img class="img-responsive logo" src="http://localhost/prometheus/public/logo.png" alt="logo">
-	        </div>
-	            
-	        <div class="row">
-	            <div class="col-md-3 gauche padding-top">
->>>>>>> Stashed changes
-	                <a href="#">Contact</a></br>
-	                <a href="#">Adresse des centres </a></br>
-	                <a href="#">Nous contacter </a></br>
-	                <a href="#">E-mail </a>
-	            </div>
-
-	            <div class="col-md-4">
-	                <p> Adresse de l'association :</br>
-	                    Prometheus eXia.Cesi</br>
-	                    2 Allée des Foulons</br>
-	                    67380 Strasbourg Lingolsheim</p>
-	            </div>
-        	</div>
+                <div class="col-md-4">
+                    <p> Adresse de l'association :<br>
+                        Prometheus eXia.Cesi<br>
+                        2 Allée des Foulons<br>
+                        67380 Strasbourg Lingolsheim</p>
+                </div>
+            </div>
         </div>
     </footer>
+    
 </body>
 
 </html>
