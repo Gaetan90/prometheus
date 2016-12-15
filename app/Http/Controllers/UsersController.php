a4548cd7a6bb16b0a940f24d50475421274052ba
Hilau <hugo.huilier@viacesi.fr>
2016-12-15 14:46:40 +0100

    Corrections

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,23 +1,77 @@
-<footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    
-                </div>
+<?php
 
-                <div class="col-md-4">
-                    <a href="#">Contact</a></br>
-                    <a href="#">Adresse des centres </a></br>
-                    <a href="#">Nous contacter </a></br>
-                    <a href="#">E-mail </a>
-                </div>
+namespace App\Http\Controllers;
 
-                <div class="col-md-4">
-                    <p> Adresse de l'association :</br>
-                        Prometheus eXia.Cesi</br>
-                        2 Allée des Foulons</br>
-                        67380 Strasbourg Lingolsheim</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UsersController extends Controller
+{
+    public function login()
+    {
+        if(Auth::check())
+        {
+            Auth::logout();
+        }
+
+    	return view('login');
+    }
+
+    public function authenticate(Request $request)
+    {
+    	$email = $request->input('email');
+    	$password = $request->input('password');
+
+    	if(Auth::attempt(['email' => $email, 'password' => $password], 1)) 
+    	{        	
+            return redirect()->route('users.index');
+        }
+
+        else
+        {
+        	return redirect('users/login')->with('alert-fail', 'Identifiants incorrect !');
+        }
+    }
+
+    public function index()
+    {
+        $user = Auth::user();
+
+        return view('users.index')->with(['user'=>$user]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect()->route('index')->with('alert-success', 'Vous êtes déconnecté, à bientôt !');
+    }
+
+    public function parameters()
+    {
+        $user = Auth::user();
+
+        return view('users.parameters')->with(['user'=>$user]);
+    }
+
+    public function parametersEdit(Request $request)
+    {
+        $new_tel = $request->input('tel');
+        $new_annee = $request->input('annee');
+
+        $user = Auth::user();
+        $user->tel = $new_tel;
+        $user->annee = $new_annee;
+        $user->save();
+
+        return redirect()->route('users.parameters')->with('alert-success', 'Les modifications ont été prises en compte.');
+    }
+
+    public function trombinoscope()
+    {
+        $user = Auth::user();
+
+        return view('users.trombinoscope')->with(['user'=>$user]);
+    }
+}
